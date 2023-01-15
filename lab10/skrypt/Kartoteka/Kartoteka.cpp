ecaeb51857fd85f05181dfb2261552b3337a485d
Bunzu013 <83347605+Bunzu013@users.noreply.github.com>
2023-01-15 02:10:45 +0100

    Add files via upload

---
+++ lab10/skrypt/Kartoteka/Kartoteka.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <regex>
+#include <fstream>
+
+using namespace std;
+
+bool check(string imie1, string nazwisko1, string wiek1, string telefon1, string email1, regex namePattern, regex nazwPattern, regex agePattern, regex telPattern, regex emailPattern, smatch res) {
+    bool temp = true;
+    regex_search(imie1, res, namePattern);
+    if (res.size() != 0) {
+
+    }
+    else {
+        temp = false;
+        cout << "Bledne imie" << endl;
+
+    }
+    regex_search(nazwisko1, res, nazwPattern);
+    if (res.size() != 0) {
+
+    }
+    else {
+        temp = false;
+        cout << "Bledne nazwisko" << endl;
+
+    }
+    regex_search(wiek1, res, agePattern);
+    if (res.size() != 0) {
+
+    }
+    else {
+        temp = false;
+        cout << "Bledny wiek" << endl;
+
+    }
+    regex_search(telefon1, res, telPattern);
+    if (res.size() == 0 || telefon1.size() != 9) {
+        temp = false;
+        cout << "Bledny numer telefonu" << endl;
+    }
+    regex_search(email1, res, emailPattern);
+    if (res.size() != 0) {
+
+    }
+    else {
+        temp = false;
+        cout << "Bledny email" << endl;
+    }
+    return temp;
+}
+
+int main()
+{
+    string imie;
+    string nazwisko;
+    string wiek;
+    string telefon;
+    string email;
+
+    smatch res;
+
+    regex namePattern("[A-Z][a-z]+");
+    regex nazwPattern("[A-Z][a-z]+(-[A-Z]?[a-z]+)?");
+    regex agePattern("[0-99]");
+    regex telPattern("[1-9][0-9]{8}");
+    regex emailPattern("[A-za-z]([A-za-z]|[0-9]|_|-|\\.)*@([A-za-z]|[0-9])+\\.(com|pl|org)");
+
+    cout << "Podaj imie: ";
+    getline(cin, imie);
+    cout << "\nPodaj nazwisko: ";
+    getline(cin, nazwisko);
+    cout << "\nPodaj wiek: ";
+    getline(cin, wiek);
+    cout << "\nPodaj numer telefonu: ";
+    getline(cin, telefon);
+    cout << "\nPodaj adres email: ";
+    getline(cin, email);
+
+    bool good = check(imie, nazwisko, wiek, telefon, email, namePattern, nazwPattern, agePattern, telPattern, emailPattern, res);
+    if (good) {
+        ofstream fileOf("lista.txt", ios::app);
+        if (fileOf.is_open()) {
+            fileOf << imie << ";" << nazwisko << ";" << wiek << ";" << telefon << ";" << email << ";" << endl;
+            fileOf.close();
+            cout << "Dane zapisano do pliku lista.txt" << endl;
+        }
+        else cout << "Blad przy otwieraniu pliku" << endl;
+    }
+    else cout << "Bledne dane" << endl;
+
+
+}
---
+++ lab10/skrypt/skrypt_regularne/skrypt_regularne.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <regex>
+
+using namespace std;
+int main()
+{
+	string line="Programowanie w C++";
+	if (regex_match (line, regex("(Prog)(.*)") ))
+	cout << "Znaleziono"<<endl;
+	else
+	cout<<"Nie znaleziono!"<<endl;
+	regex regPattern("(Prog)(.*)");
+	if (regex_match (line, regPattern ))
+	cout << "Znaleziono"<<endl;
+	else
+	cout<<"Nie znaleziono!"<<endl;
+	if (regex_match (line.begin(), line.end(),regPattern ))
+	cout << "Znaleziono"<<endl;
+	else
+	cout<<"Nie znaleziono!"<<endl;
+	smatch res;
+	regPattern="(Prog)(.*)";
+	regex_match(line,res,regPattern);
+	cout<<"Regex_match: "<<endl;
+	cout << "Czy znaleziony? " << res.size()<<endl;
+	if(res.size()!=0)
+	cout<<"Wzorzec: "<<res[0]<<
+	" zostal znaleziony"<<endl;
+	cout<<endl<<"Porownanie match i search"<<endl;
+	line="*******Programowanie w C++********";
+	regPattern="(Prog)(.*)";
+	regex_match(line,res,regPattern);
+	cout<<"Regex_match: "<<endl;
+	cout << "Czy znaleziony? " << res.size()<<endl;
+	if(res.size()!=0)
+	cout<<"Wzorzec: "<<res[0]<<
+	" zostal znaleziony"<<endl;
+	regex_search(line,res,regPattern);
+	cout<<"Regex_search: "<<endl;
+	cout << "Liczba dopasowan:" << res.size()<<endl;
+	cout << "res.str(): " << res.str() << endl;
+	cout << "res.length(): " << res.length() << endl;
+	cout << "res.position(): " << res.position() << endl;
+	cout<<"res.prefix: "<<res.prefix().str()<<endl;
+	cout<<"res.suffix: "<<res.suffix().str()<<endl;
+	cout << endl;
+	cout<<"Podgrupy ver1:"<<endl;
+	for (int i=0; i<res.size(); ++i)
+	{
+		cout << "[" << res[i] << "] ";
+		cout << "res.str(): " << res.str(i) << endl;
+		cout << "res.position(): "<<res.position(i)<< endl;
+		cout<<"res.dl: "<<res.length(i)<<endl;
+		cout << endl;
+	}
+	cout << "Podgrupy ver2:" << endl;
+	for (auto pos = res.begin(); pos != res.end(); pos++)
+	{
+		cout << "[" << *pos <<"] ";
+		cout << "dl: " << pos->length() << endl;
+	}
+	cout<<endl<<"Replace: "<<endl;
+	line="*******Programowanie w C++********";
+	regPattern="(C\\+{2})";
+	cout<<"Przed replace: "<<line<<endl;
+	line=regex_replace(line, regPattern, "Java");
+	cout<<"Po replace: "<<line<<endl;
+	return 0;
+}
